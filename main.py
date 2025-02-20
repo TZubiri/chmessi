8f22132af40de081a0f3a71eb165879ea04c321c
Tom Zubiri <tomzubiri@openscreensit.com>
2025-02-20 06:01:22 -0300

    ai signatures

--- main.py
+++ main.py
@@ -1,26 +1,30 @@
 class Player:
-	def make_move(board,move):
+	def make_move(board,move:str):
 		pass
 
 class Board:
-	def setup(Board):
+	def setup(self):
 		pass
 
 class Arbiter:
-	def is_legal(board,move,log) -> bool:
+	def is_legal(board:Board,move:str,log) -> bool:
 		pass
 
 class Log:
 	pass
 
 class Player:
-	def log_move(move):
+	def log_move(move:str):
 		self.moves.append(move)
 
 class Rules:
 	def initial_setup():
 		pass
-	def is_legal(board,move,log):
+	def is_legal(board:Board,move:str,log:Log):
 		pass
 	def result(): #-> Enum("1-0","0-1","1/2-1/2","live")
 		pass
+
+class AI:
+	def legal_moves(board:Board,log)->[str]:
+		...
